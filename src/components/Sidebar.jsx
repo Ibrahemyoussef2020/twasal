2103ec7bb25607fee67fb021b5f9b2c3e5917b20
Ibrahemyoussef2020 <ibrahem.youssef2020@hotmail.com>
2024-06-30 19:28:57 +0400

    re build sidebar

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -4,18 +4,20 @@ import { IoHomeSharp } from "react-icons/io5";
 
 const Sidebar = () => {
   return (
-    <aside className="dir-r pr-3 flex flex-col items-end w-full">
-      <Link href='/' className="flex justify-end">
-         <FaXTwitter className="w-16 h-16 cursor-pointer p-3 hover:bg-gray-100 rounded-full transition-all duration-200" />
+    <aside className="dir-r pr-3 flex flex-col items-start w-full">
+      <Link href='/' className="flex justify-start text-3xl pr-0 py-4 font-extrabold text-green-700  relative">
+      <span className="pr-4"> تَوَاصَل ْ</span>
       </Link>
 
-      <Link href='/' className="flex justify-end ml-0 mr-auto items-center	 gap-2 p-3 hover:bg-gray-100 rounded-full transition-all duration-200 w-fit">
-        <span className="font-bold hidden xl:inline">Home</span>
-         <IoHomeSharp  className="w-7 h-7" />
+      <Link href='/' className="flex justify-start ml-auto mr-0 items-center	 gap-2 p-3 hover:bg-gray-100 rounded-full transition-all duration-200 w-fit">
+        <IoHomeSharp  className="w-7 h-7" />
+        <span className="font-bold hidden lg:inline text-lg">
+          الرئيسية 
+        </span>
       </Link>
 
-      <button className="hidden xl:inline bg-blue-400 text-white font-bold rounded-full py-2 px-4 mt-4 w-48 hover:bg-blue-600 transition-all duration-200 shadow-md">
-        Sign In
+      <button className="hidden lg:inline bg-green-600 text-white font-semibold rounded-full py-2 px-4 mt-4 w-48 hover:bg-green-700 transition-all duration-200 shadow-md">
+        تسجيل الدخول
       </button>
     </aside>
   )
